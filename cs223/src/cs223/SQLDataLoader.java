8e6ea5377ff73b68a34f62346f57014c3f461f9f
nanor <ttqfwfmg@gmail.com>
2023-05-09 17:18:51 -0700

    initial commit

---
+++ cs223/src/cs223/SQLDataLoader.java
@@ -0,0 +1,12 @@
+package cs223;
+
+import java.util.HashMap;
+
+public class SQLDataLoader {
+    public static void PreprocessInserts(String filename, HashMap<Long, String[]> sqlMap) {
+        return;
+    }
+
+    public static void RunSQLByLine(String s) {
+    }
+}
---
+++ cs223/src/cs223/Settings.java
@@ -0,0 +1,52 @@
+package cs223;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Settings {
+
+//    public static int MPL = 10;
+//
+//
+//
+//    public static int ISOLATION_LEVEL = Connection.TRANSACTION_SERIALIZABLE;
+//
+//    public static int INTERVAL_BETWEEN_TIME_UNIT = 1000; // Do not change
+//
+//    public static boolean DO_NOT_GROUP_DATA_OPERATIONS = false;
+//
+//    public final static String PREPROCESSED_DATA_URL = "preprocessed/"; //Do not change
+//
+//    public static int TEST_RUNNING_TIME_SECS = 3;
+    public static List<Integer> MPLS = new ArrayList<Integer>(Arrays.asList(2, 5, 10, 20));
+
+    public static List<Integer> TRANSACTION_SIZE = new ArrayList<Integer>(Arrays.asList(1, 4, 10, 25));
+
+    public static List<Integer> LEVELS = new ArrayList<>(Arrays.asList(Connection.TRANSACTION_READ_UNCOMMITTED,
+            Connection.TRANSACTION_READ_COMMITTED,
+            Connection.TRANSACTION_REPEATABLE_READ,
+            Connection.TRANSACTION_SERIALIZABLE));
+
+    public static int PERIOD = 100; // simulation time step in millisecond
+
+    public static long SIMULATION_LENGTH = 120000; // in millisecond
+
+    public static String SEMANTIC_DATASET_URL = "Resources/data/low_concurrency/semantic_observation_low_concurrency.sql";
+
+    public static String OBSERVATION_DATASET_URL = "Resources/data/low_concurrency/observation_low_concurrency.sql";
+
+    public static String METADATA_DATASET_URL = "Resources/data/low_concurrency/metadata.sql";
+
+    public static String QUERY_DATA_URL = "Resources/queries/low_concurrency/queries.txt";
+
+    public static boolean HIGH_CONCURRENCY = false;
+
+    public static void switch_to_high_concurrency() {
+        SEMANTIC_DATASET_URL = "Resources/data/high_concurrency/semantic_observation_high_concurrency.sql";
+        OBSERVATION_DATASET_URL = "Resources/data/high_concurrency/observation_high_concurrency.sql";
+        METADATA_DATASET_URL = "Resources/data/high_concurrency/metadata.sql";
+        QUERY_DATA_URL = "Resources/queries/high_concurrency/queries.txt";
+    }
+}
